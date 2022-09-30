d9b77c554ea08d2eafcfd157023d78ea1c9d7de8
thais.batista <thais.batista@maximatech.com.br>
2022-09-30 14:21:07 -0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
+import { ExtratoComponent } from './extrato/extrato.component';
+import { NovaTransferenciaComponent } from './nova-transferencia/nova-transferencia.component';
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { LOCALE_ID, NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { registerLocaleData } from '@angular/common';
+import localePt from '@angular/common/locales/pt';
+import { HttpClientModule } from '@angular/common/http';
+import { AppRountingModule } from './app-routing.module';
+
+registerLocaleData(localePt, 'pt')
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [AppComponent, NovaTransferenciaComponent, ExtratoComponent],
+  imports: [BrowserModule, FormsModule, HttpClientModule, AppRountingModule],
+  providers: [{ provide: LOCALE_ID, useValue: "pt" }],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
